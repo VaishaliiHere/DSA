ba6a231d85b5f2dd9c19b03017754787813a3840
VaishaliiHere <vaishalish643@gmail.com>
2026-07-12 19:18:57 +0530

    Time: 1 ms (50.21%) | Memory: 43.5 MB (21.77%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s == goal)
+            return true;
+        for (int i = 0; i < s.length(); i++) {
+
+           String rotated = s.substring(i) + s.substring(0, i);
+
+            if (rotated.equals(goal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
